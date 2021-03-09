b23f4e5b5d0bf09dc6b79b3957c007e4169be0f9
nuel <66974415+nuel@users.noreply.github.com>
2021-03-09 19:41:17 +0100

    Add typing indicators and command parsing

--- server/server.js
+++ server/server.js
@@ -1,3 +1,12 @@
+const fs = require('fs')
+const secrets = {}
+
+try {
+  secrets.key = fs.readFileSync('.key', 'utf8')
+} catch (err) {
+  console.error(err)
+}
+
 const io = require('socket.io')({
     cors: {
         origin: '*'
@@ -5,14 +14,62 @@ const io = require('socket.io')({
 })
 const server = io.listen(8443)
 
-const mods = []
+const clients = {}
 
 server.on('connection', socket => {
+    // Broadcast client list and own id on login
+    clients[socket.client.id] = {}
     socket.emit('remote-id', socket.client.id)
+    server.emit('clients', clients)
+
     socket.on('chat-message', message => {
-        server.emit('chat-message', {
-            message,
-            id: socket.client.id
-        })
+        // Handle commands
+        if (message.content && message.content[0] === '/')  {
+            const argv = message.content.slice(1, message.content.length).split(' ')
+
+            if (argv.length > 1) {
+                switch (argv[0]) {
+                    case 'mod':
+                        if (argv[1] === secrets.key) {
+                            // Check if key is correct, if so, tell everyone we have a mod here
+                            clients[socket.client.id].mod = true
+                            server.emit('clients', clients)
+                        }
+                        break
+                    case 'ban':
+                        if (argv[1] && clients[argv[1]]) {
+                            console.log(clients[argv[1]])
+                        }
+                        break
+                    default:
+                        break
+                }
+            }
+        } else {
+            server.emit('chat-message', {
+                message,
+                id: socket.client.id
+            })
+        }
+    })
+    socket.on('set-name', message => {
+        clients[socket.client.id].tjetName = message
+    })
+    socket.on('typing', message => {
+        // If not already typing, change status and broadcast updated list
+        if (!clients[socket.client.id].typing) {
+            clients[socket.client.id].typing = true
+            server.emit('clients', clients)
+        }
+    })
+    socket.on('idle', message => {
+        // Update status and send new list to everyone
+        clients[socket.client.id].typing = false
+        server.emit('clients', clients)
+    })
+    socket.on('disconnect', () => {
+        // Delete this client from the list and inform everyone
+        delete clients[socket.client.id]
+        server.emit('clients', clients)
     })
 })
--- tjet.js
+++ tjet.js
@@ -28,7 +28,9 @@ function h (tagName, properties, content) {
 // State
 const tjetState = {
     name: null,
-    remoteID: null
+    remoteID: null,
+    typing: false,
+    clients: {}
 }
 
 // Localization
@@ -36,12 +38,17 @@ const tjetLabels = {
     sendLabel:  'Send',
     setNameLabel: 'Set name',
     noNamePlaceholder: 'Enter your name',
-    sendPlaceholder: 'Send a message'
+    sendPlaceholder: 'Send a message',
+    and: 'and',
+    typingSingular: 'is typing',
+    typingPlural: 'are typing',
+    typingSeveral: 'Several people'
 }
 
 // Create UI
 const tjet = document.getElementById('tjet')
 const tjetMessages = h('ul.tjet-messages')
+const tjetTypingIndicator = h('div.tjet-typing-indicator')
 const tjetForm = h('form.tjet-form.tjet-no-name', {
     action: 'javascript:;'
 })
@@ -55,6 +62,7 @@ const tjetSubmit = h('a.tjet-submit', tjetLabels.setNameLabel)
 tjetForm.appendChild(tjetInput)
 tjetForm.appendChild(tjetSubmit)
 tjet.appendChild(tjetMessages)
+tjet.appendChild(tjetTypingIndicator)
 tjet.appendChild(tjetForm)
 
 // Connect to server
@@ -79,6 +87,52 @@ socket.on('chat-message', data => {
     tjetMessages.appendChild(bubble)
 })
 
+// Client list syncing
+socket.on('clients', clients => {
+    // Update list
+    tjetState.clients = clients
+
+    // Check if we have become mod
+    if (
+        tjetState.clients[tjetState.remoteID] &&
+        tjetState.clients[tjetState.remoteID].mod
+    ) {
+        tjet.classList.add('tjet-advanced')
+    }
+
+    // Check if people are typing
+    tjetCheckTypingIndicators()
+})
+
+// Check typing indicators
+function tjetCheckTypingIndicators() {
+    const typing = []
+    for (client in tjetState.clients) {
+        if (
+            tjetState.remoteID !== client &&
+            tjetState.clients[client].typing
+        ) {
+            typing.push(client)
+        }
+    }
+    if (typing.length) {
+        tjetTypingIndicator.classList.add('active')
+
+        if (typing.length === 1) {
+            tjetTypingIndicator.innerHTML = `<span class="tjet-name">${tjetGetName(typing[0])}</span> ${tjetLabels.typingSingular}&hellip;`
+        } else if (typing.length === 2) {
+            tjetTypingIndicator.innerHTML = `<span class="tjet-name">${tjetGetName(typing[0])}</span> ${tjetLabels.and} <span class="tjet-name">${tjetGetName(typing[1])}</span> ${tjetLabels.typingPlural}&hellip;`
+        } else if (typing.length === 3) {
+            tjetTypingIndicator.innerHTML = `<span class="tjet-name">${tjetGetName(typing[0])}</span>, <span class="tjet-name">${tjetGetName(typing[1])}</span> ${tjetLabels.and} <span class="tjet-name">${tjetGetName(typing[2])}</span> ${tjetLabels.typingPlural}&hellip;`
+        } else {
+            tjetTypingIndicator.innerHTML = `<span class="tjet-name">${tjetLabels.typingSeveral}</span> ${tjetLabels.typingPlural}&hellip;`
+        }
+    } else {
+        tjetTypingIndicator.classList.remove('active')
+        tjetTypingIndicator.innerHTML = ''
+    }
+}
+
 // Handle user input
 function tjetHandleInput() {
     if(tjetState.name) {
@@ -91,11 +145,36 @@ function tjetHandleInput() {
         tjetSubmit.innerHTML = tjetLabels.sendLabel
         tjetForm.classList.remove('tjet-no-name')
         tjetInput.placeholder = tjetLabels.sendPlaceholder
+        socket.emit('set-name', tjetState.name)
     }
 
     tjetInput.value = ''
 }
 
+// Broadcast typing status on begin and end
+function tjetBroadcastTypingIndicator() {
+    if(tjetState.name) {
+        if(tjetInput.value) {
+            if (!tjetState.typing) {
+                socket.emit('typing')
+                tjetState.typing = true
+            }
+        } else {
+            socket.emit('idle')
+            tjetState.typing = false
+        }
+    }
+}
+
+// Helper function to fetch names for IDs
+function tjetGetName(id) {
+    if (tjetState.clients[id]) {
+        return tjetState.clients[id].tjetName
+    }
+    return false
+}
+
 // Bind listeners
 tjetSubmit.addEventListener('click', tjetHandleInput)
 tjetForm.addEventListener('submit', tjetHandleInput)
+tjetInput.addEventListener('keyup', tjetBroadcastTypingIndicator)
