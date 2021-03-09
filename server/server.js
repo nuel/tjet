54b06549f1bfb09f9c508fbb9106ed33d8cddb35
nuel <66974415+nuel@users.noreply.github.com>
2021-03-09 12:38:52 +0100

    Add simple h function, add ID checks

--- server/server.js
+++ server/server.js
@@ -5,8 +5,14 @@ const io = require('socket.io')({
 })
 const server = io.listen(8443)
 
+const mods = []
+
 server.on('connection', socket => {
+    socket.emit('remote-id', socket.client.id)
     socket.on('chat-message', message => {
-        server.emit('chat-message', message)
+        server.emit('chat-message', {
+            message,
+            id: socket.client.id
+        })
     })
 })
--- tjet.js
+++ tjet.js
@@ -1,6 +1,37 @@
-const tjetState = {
-    name: false
+/** Tjet
+ * by nuel
+ */
+
+// Simplified h function
+function h (tagName, properties, content) {
+    const parts = tagName.split('.')
+    const el = document.createElement(parts[0])
+
+    parts.shift()
+    parts.forEach(className => {
+        el.classList.add(className)
+    })
+
+    if (!content && typeof properties === 'string') {
+        content = properties
+    }
+
+    if (typeof properties === 'object') {
+        Object.assign(el, properties)
+    }
+
+    if (content) el.innerHTML = content
+
+    return el
 }
+
+// State
+const tjetState = {
+    name: null,
+    remoteID: null
+}
+
+// Localization
 const tjetLabels = {
     sendLabel:  'Send',
     setNameLabel: 'Set name',
@@ -8,59 +39,51 @@ const tjetLabels = {
     sendPlaceholder: 'Send a message'
 }
 
-// Create UI ------------
+// Create UI
 const tjet = document.getElementById('tjet')
-const tjetMessages = document.createElement('ul')
-tjetMessages.classList.add('tjet-messages')
+const tjetMessages = h('ul.tjet-messages')
+const tjetForm = h('form.tjet-form.tjet-no-name', {
+    action: 'javascript:;'
+})
+const tjetInput = h('input.tjet-input', {
+    name: 'tjet-input',
+    placeholder: tjetLabels.noNamePlaceholder
+})
+const tjetSubmit = h('a.tjet-submit', tjetLabels.setNameLabel)
 
-// Form
-const tjetForm = document.createElement('form')
-tjetForm.action = 'javascript:;'
-tjetForm.classList.add('tjet-form')
-tjetForm.classList.add('tjet-no-name')
-
-// Input box
-const tjetInput = document.createElement('input')
-tjetInput.name = 'tjet-input'
-tjetInput.placeholder = tjetLabels.noNamePlaceholder
-tjetInput.classList.add('tjet-input')
-
-// Submit button
-const tjetSubmit = document.createElement('a')
-tjetSubmit.innerHTML = tjetLabels.setNameLabel
-tjetSubmit.classList.add('tjet-submit')
-
-// Put them all together
-tjet.appendChild(tjetMessages)
+// Attach UI
 tjetForm.appendChild(tjetInput)
 tjetForm.appendChild(tjetSubmit)
+tjet.appendChild(tjetMessages)
 tjet.appendChild(tjetForm)
-// End create UI ------------
 
 // Connect to server
 const socket = io(tjet.dataset.server || null)
 
+// Listen for remote id to be set
+socket.on('remote-id', data => {
+    tjetState.remoteID = data
+})
+
+// Handle incoming messages
 socket.on('chat-message', data => {
-    const bubble = document.createElement('li')
-    const name = document.createElement('div')
-    const message = document.createElement('div')
+    const bubble = h('li.tjet-message')
+    const messageName = h('div.tjet-message-name', data.message.name)
+    const senderId = h('div.tjet-sender-id', data.id)
+    const messageContent = h('div.tjet-message-content', data.message.content)
+    if (tjetState.remoteID === data.id) bubble.classList.add('tjet-sent')
 
-    bubble.classList.add('tjet-message')
-    name.classList.add('tjet-message-name')
-    name.innerHTML = data.name
-    message.classList.add('tjet-message-content')
-    message.innerHTML = data.message
-
-    bubble.appendChild(name)
-    bubble.appendChild(message)
+    bubble.appendChild(messageName)
+    bubble.appendChild(senderId)
+    bubble.appendChild(messageContent)
     tjetMessages.appendChild(bubble)
 })
 
-// Handle inputs
+// Handle user input
 function tjetHandleInput() {
     if(tjetState.name) {
         socket.emit('chat-message', {
-            message: tjetInput.value,
+            content: tjetInput.value,
             name: tjetState.name
         })
     } else {
